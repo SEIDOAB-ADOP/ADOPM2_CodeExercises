7bddeb7e81aebfcffd8fcb7300eced14fd4ccf0c
Martin Lenart <89787032+martinlenart@users.noreply.github.com>
2024-11-28 12:36:41 +0100

    11_interfaces added

---
+++ 11_interfaces/Models/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Seido.Utilities.SeedGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_interfaces.Models
+{
+    public enum CarBrand { Boxcar, Ford, Jaguar, Honda, Mercedes }
+    public enum CarModel { Boxmodel, Mustang_GT, Golf, V70, XF, Civic, S_class, E_class }
+    public interface ICar
+    {
+        public CarBrand Brand { get; set; }
+        public CarModel Model { get; set; }
+        public string RegNumber { get; set; }
+        public int Year { get; set; }
+
+        public IPerson Owner { get; set; }
+        public ICar Seed(SeedGenerator seeder);
+    }
+
+    public interface IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public IPerson Seed(SeedGenerator _seeder);
+    }
+}
---
+++ 11_interfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/* Exercises
+
+1. Declare a class PersonAsClass that implements IPerson. Seed() should set random values to all props
+2. Declare a class CarAsClass that implements ICar. Seed() should set random values to all props
+   and use new PersonAsClass() when instatiating IPerson.
+3. Use GetType() in ToString() to show the type of the ICar when presenting the car
+4. In Program.cs create a list ICar, that should contain 50 cars
+5. Print to the console the 10 oldest and youngest cars
+
+6. Declare a struct PersonAsStruct that implements IPerson. Seed() should set random values to all props
+7. Declare a struct CarAsStruct that implements ICar. Seed() should set random values to all props
+   and use new PersonAsStruct() when instatiating IPerson.
+8. Use GetType() in ToString() to show the type of the ICar when presenting the car
+9. Add 50 cars of CarAsStruct to the list of cars.
+10. Print to the console the 10 oldest and youngest cars
+
+ */
