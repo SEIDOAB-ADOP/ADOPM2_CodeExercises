a44e24079b619966801d1a3c1cef5f6dec243bea
Martin <martin_lenart@icloud.com>
2024-12-02 10:48:06 +0100

    update

---
+++ 14_tuples/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+
+public enum PearlColor { Black, White, Pink }
+public enum PearlShape { Round, DropShaped }
+public enum PearlType { FreshWater, SaltWater }
+
+
+public enum PlayingCardSuit {Clubs = 0, Diamonds, Hearts, Spades}
+public enum PlayingCardValue { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Knight, Queen, King, Ace}
+
+/* Exercises
+1. Create a tuple pearl1 which is Black, Round, Saltwater
+2. Create a tuple pearl2 which is White, DropShaped, Freashwater
+3. Use tuple syntax to test if pearl1 is equal to pearl2
+
+4. Modify pearl1 and pearl2 tuple creatation so the tuple items are no longer calles Item1, Item2, Item3 but
+   Color, Shape and Type
+
+5. Use tuple syntax to create pearl3 that is a copy of pearl1
+6. Use tuple syntax to create three variables PearlColor Color, PearlShape Shape and PearlType Type from pearl2
+
+7. Create a list of tuples List <(PlayingCardSuit, PlayingCardValue)> and fill it with 52 cards lika a deck of card
+8. Write the deck of card to the console 
+
+*/
